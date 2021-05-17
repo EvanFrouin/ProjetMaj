990ce6fec840e4499f0260d4d8d0692678ef0f86
alexandremarcq <alexandremarcq58@gmail.com>
2021-05-17 19:05:19 +0200

    [Updated]:[refactored to include new methods]

--- web/__init__.py
+++ web/__init__.py
@@ -1,12 +1,10 @@
 from flask import Flask
 from flask_login import LoginManager
-from flask_mongoengine import MongoEngine
-import random
-
-db = MongoEngine()
 
 
 def create_app():
+    from .db import db
+
     app = Flask(__name__)
 
     app.config['SECRET_KEY'] = 'secret-key'
@@ -19,20 +17,23 @@ def create_app():
 
     db.init_app(app)
 
+    from .db import User, Patient, init_patients
+
+    if len(Patient.objects) == 0:
+        init_patients()
+
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
-
-    from .models import User
 
     @login_manager.user_loader
     def load_user(user_id):
         return User.objects(pk=user_id).first()
 
-    from .auth import auth as auth_blueprint
+    from web.routes.auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
 
-    from .main import main as main_blueprint
+    from web.routes.main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     from flask_socketio import SocketIO
