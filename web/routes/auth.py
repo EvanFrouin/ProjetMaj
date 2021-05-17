bfb2a04113e31799b11479c61031425288e71a6b
alexandremarcq <alexandremarcq58@gmail.com>
2021-05-17 19:02:55 +0200

    [Updated]:[included database methods]

--- web/routes/auth.py
+++ web/routes/auth.py
@@ -1,13 +1,13 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from flask_login import login_user, logout_user, login_required
-from .models import User
+from ..db import User, get_user_by_email, insert_user
 
 auth = Blueprint('auth', __name__)
 
 
 @auth.route('/login')
 def login():
-    return render_template("login.html")
+    return render_template("auth/login.html")
 
 
 @auth.route('/login', methods=['POST'])
@@ -15,35 +15,34 @@ def login_post():
     email = request.form.get('email')
     password = request.form.get('password')
 
-    user = User.objects(email=email).first()
+    user = get_user_by_email(email)
 
     if not user or password != user.password:
         flash('Wrong credentials, please try again')
         return redirect(url_for('auth.login'))
 
     login_user(user)
-    return redirect(url_for('main.profile'))
+    return redirect(url_for('main.dashboard'))
 
 
 @auth.route('/signup')
 def signup():
-    return render_template("signup.html")
+    return render_template("auth/signup.html")
 
 
 @auth.route('/signup', methods=['POST'])
 def signup_post():
     email = request.form.get('email')
-    name = request.form.get('name')
+    name = request.form.get('username')
     password = request.form.get('password')
 
-    user = User.objects(name=name).first()
+    user = get_user_by_email(email)
 
     if user:
         flash('This email address is already in use')
         return redirect(url_for('auth.signup'))
 
-    user = User(email=email, name=name, password=password)
-    user.save()
+    insert_user(email, name, password)
 
     return redirect(url_for('auth.login'))
 
@@ -52,4 +51,4 @@ def signup_post():
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('main.index'))
+    return redirect(url_for('auth.login'))
