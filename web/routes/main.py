57d7338cb642a59db5c43a8864bcce7d172a24c5
Alexandre Marcq <alexandremarcq58@gmail.com>
2021-05-20 17:02:50 +0200

    [Updated]:[main.py to add the search POST route]

--- web/routes/main.py
+++ web/routes/main.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_login import login_required, current_user
+from ..db import get_patient_by_query
 
 main = Blueprint('main', __name__)
 
@@ -20,3 +21,10 @@ def profile():
 @login_required
 def search():
     return render_template("search.html")
+
+
+@main.route('/search', methods=['POST'])
+@login_required
+def patient_query():
+	query = request.form.get('query')
+	return render_template("search.html", results=get_patient_by_query(gender=query))
